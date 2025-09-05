dbb896024c6c899e539f284ba06ca2bc693dd208
eils155129-svg <eils155129@elevate208.org>
2025-09-04 22:03:51 -0600

    Update debloat.sh

--- debloat.sh
+++ debloat.sh
@@ -2,6 +2,6 @@
 echo "This will remove all gnome packages except for the desktop itself,extensions,settings,and hardware monitor. "
 
 
-sudo apt remove evolution evolution-plugins gnome-sound-recorder libreoffice-calc libreoffice-gnome libreoffice-gtk3 libreoffice-impress libreoffice-writer shotwell totem totem-plugins gnome-music mythes-en-us libreoffice-help-en-us hunspell-en-us gnome-games gnome-terminal gnome-maps gnome-text-editor gnome-calendar gnome-clocks gnome-connections gnome-contacts gnome-software gnome-weather gnome-tour gnome-software gnome-calculator gnome-console gnome-genius gnome-chess loupe gnome-mahjongg gnome-online-accounts gnome-remote-desktop gnome-chess 
+sudo apt remove evolution evolution-plugins gnome-sound-recorder libreoffice-calc libreoffice-gnome libreoffice-gtk3 libreoffice-impress libreoffice-writer shotwell totem totem-plugins gnome-music mythes-en-us libreoffice-help-en-us hunspell-en-us gnome-games gnome-terminal gnome-maps gnome-text-editor gnome-calendar gnome-clocks gnome-connections gnome-contacts gnome-software gnome-weather gnome-tour gnome-software gnome-calculator gnome-console gnome-genius gnome-chess loupe gnome-mahjongg gnome-online-accounts gnome-remote-desktop gnome-chess gnome-camera gnome-sushi gnome-document-viewer gnome-simple-scan 
 sudo apt autoremove
-sudo apt install kitty starship mkv kate
+sudo apt install kitty starship  kate
