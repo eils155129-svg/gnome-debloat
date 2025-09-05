43c2783d30c2f433b592291cbf8c2226b7523d89
eils155129-svg <eils155129@elevate208.org>
2025-09-05 01:40:42 +0000

    Added debloat

--- debloat.sh
+++ debloat.sh
@@ -4,3 +4,4 @@ echo "This will remove all gnome packages except for the desktop itself,extensio
 
 sudo apt remove evolution evolution-plugins gnome-sound-recorder libreoffice-calc libreoffice-gnome libreoffice-gtk3 libreoffice-impress libreoffice-writer shotwell totem totem-plugins gnome-music mythes-en-us libreoffice-help-en-us hunspell-en-us gnome-games gnome-terminal gnome-maps gnome-text-editor gnome-calendar gnome-clocks gnome-connections gnome-contacts gnome-software gnome-weather gnome-tour gnome-software gnome-calculator gnome-console gnome-genius gnome-chess loupe gnome-mahjongg gnome-online-accounts gnome-remote-desktop gnome-chess 
 sudo apt autoremove
+sudo apt install kitty starship mkv 
